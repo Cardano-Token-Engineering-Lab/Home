282c56f57561342020b64c4a79f207be7927aa9b
USCMigs <miguel.robert.saldana@gmail.com>
2023-07-30 00:04:55 -0500

    Update token_tx.py with initial plot

--- Query Cardano Data/Blockfrost/token_tx.py
+++ Query Cardano Data/Blockfrost/token_tx.py
@@ -1,9 +1,8 @@
-from blockfrost import BlockFrostApi, ApiError, ApiUrls
+from blockfrost import BlockFrostApi
 import pandas as pd
 import os
-from dotenv import load_dotenv, dotenv_values
-from openpyxl import Workbook
-from openpyxl.utils.dataframe import dataframe_to_rows
+from dotenv import load_dotenv
+import matplotlib.pyplot as plt
 
 load_dotenv()
 api_key = os.getenv("BLOCKFROST_API_TOKEN")
@@ -15,19 +14,64 @@ asset_name = "4c51"
 
 token_name = bytearray.fromhex(asset_name).decode()
 
-page = 1
-count = 100
-df_asset_tx = pd.DataFrame()
+def unix_to_time(df):
+    df['time'] = pd.to_datetime(df['block_time'], unit='s')
+    return df
 
-while True:
-    data = api.asset_transactions(asset=policy_id + asset_name, page=page, return_type="pandas")
-    df_asset_tx = pd.concat([data,df_asset_tx])
-    if len(data) < 100:
-#    if page > 3:
-        break
-    page += 1
+def get_asset_tx(policy_id, asset_name, page):
 
-def write_csv(dataframe, file_path):
-    dataframe.to_csv(file_path, index=False)
+    page = 1
+    count = 100
+    df_asset_tx = pd.DataFrame()
 
-write_csv(df_asset_tx, token_name+"_tx_data.csv")
+    while True:
+        data = api.asset_transactions(asset=policy_id + asset_name, page=page, return_type="pandas")
+        df_asset_tx = pd.concat([data,df_asset_tx])
+        if len(data) < 100:
+    #    if page > 3:
+            break
+        page += 1
+    
+    df_asset_tx = unix_to_time(df_asset_tx)
+    
+    return df_asset_tx
+
+def write_csv(df, file_path):
+    df.to_csv(file_path, index=False)
+
+#write_csv(df_asset_tx, token_name+"_tx_data.csv")
+
+def read_csv(filepath):
+    filename = token_name+"_tx_data.csv"
+    df = pd.read_csv(filename)
+    return df
+
+df_tx_data = read_csv(token_name+"_tx_data.csv")
+
+def histogram(df, token_name):
+    df['year_month'] = df['time'].dt.to_period('M')
+    monthly_events = df.groupby('year_month').size()
+    plt.figure(figsize=(10, 6))
+    monthly_events.plot(kind='bar', color='skyblue')
+    plt.xlabel('Time')
+    plt.ylabel('Token Tx per month')
+    plt.title(token_name+' Monthly Tx counts since launch')
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.show()
+
+def process_tx_data(data, token_name):
+    if type(data) == str:
+        df = read_csv(data)
+    else:
+        df = data
+    df = unix_to_time(df)
+    df.sort_values(by=['time'])
+    histogram(df, token_name)
+
+process_tx_data(token_name+"_tx_data.csv", token_name)
+
+
+
+
+
